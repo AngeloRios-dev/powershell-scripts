7aed675b5e5ebdcf0906bc5ad500cbb5f5167dce
Angelo Rios <angelorios@live.com>
2024-03-14 23:48:58 +0100

    fix: codigo organizado por funciones

--- ADuser/ADuser.ps1
+++ ADuser/ADuser.ps1
@@ -2,34 +2,112 @@
     Script: ADuser.ps1
     Autor: Angelo Rios
 
-    Descripción: Este script de PowerShell proporciona acciones para mejorar la eficiencia al
+    Descripcion: Este script de PowerShell proporciona acciones para mejorar la eficiencia al
     automatizar procesos repetitivos de administracion de usuarios en el Directorio Activo.
     Desarrollado pensando en las funcionalidades que necesito en mi entorno de trabajo.
 
 
-    Las acciones disponibles incluyen 
-    - verificar el estado e información básica de un usuario, 
-    - desbloquear un usuario en Active Directory, 
-    - mostrar los grupos de pertenencia del usuario y 
-    - cambiar la contraseña del usuario.
+    Las acciones disponibles son:
+    - Verificar el estado e informacion basica de un usuario, 
+    - Desbloquear un usuario en Directorio Activo, 
+    - Mostrar los grupos de pertenencia del usuario y 
+    - Cambiar la contraseña del usuario.
 
-    Fecha de creación: 21 de febrero de 2023
-    Última modificación: 12 de marzo de 2024
+    Fecha de creacion: 21 de febrero de 2023
+    Ultima modificación: 12 de marzo de 2024
 #>
 
 # Importar el modulo Active Directory
 Import-Module ActiveDirectory
 
+# Funcion para verificar información del usuario
+function VerificarUsuario {
+    param(
+        [string]$nombreUsuario
+    )
+    cls
+    Write-Host "Verificando informacion del usuario:"
+    $usuario = Get-ADUser -Identity $nombreUsuario -Properties Lockedout
+
+    if ($usuario.LockedOut) {
+        Write-Host "El usuario esta bloqueado. Desbloqueando..."
+        Unlock-ADAccount -Identity $nombreUsuario
+        Write-Host "Usuario desbloqueado exitosamente."
+    }
+
+    # Mostrar información del usuario
+    $usuario = Get-ADUser -Identity $nombreUsuario -Properties EmployeeID, GivenName, Surname, UserPrincipalName, Enabled, Lockedout, PasswordExpired, PasswordLastSet, Created, AccountExpirationDate
+    $usuario | Select-Object EmployeeID, GivenName, Surname, UserPrincipalName, Enabled, Lockedout, PasswordExpired, PasswordLastSet, Created, AccountExpirationDate
+}
+
+# Funcion para desbloquear usuario
+function DesbloquearUsuario {
+    param(
+        [string]$nombreUsuario
+    )
+
+    Write-Host "Desbloqueando $nombreUsuario en AD"
+    Unlock-ADAccount -Identity $nombreUsuario
+    # Esperar antes de verificar el estado
+    Start-Sleep -Seconds 2
+    # Obtener y mostrar el estado del usuario
+    $userStatus = Get-ADUser -Identity $nombreUsuario -Properties Lockedout | Select-Object -ExpandProperty Lockedout
+
+    if ($userStatus -eq $false) {
+        Write-Host "Usuario desbloqueado exitosamente."
+    } else {
+        Write-Host "Ha habido un error. Por favor, volver a intentarlo."
+    }
+    Write-Host ""
+}
+
+# Funcion para mostrar grupos de pertenencia
+function MostrarGruposPertenencia {
+    param(
+        [string]$nombreUsuario
+    )
+
+    Write-Host "Grupos a los que pertenece:"
+    # Buscar los grupos de los que es miembro el usuario
+    $memberOfGroups = Get-ADUser -Identity $nombreUsuario -Properties MemberOf | Select-Object -ExpandProperty MemberOf
+    # Mostrar los nombres de los grupos
+    foreach ($group in $memberOfGroups) {
+        $groupName = ($group -split ',')[0] -replace '^CN='
+        Write-Host "- $groupName"
+    }
+}
+
+# Funcion para cambio de contraseña
+function CambiarContraseña {
+    param(
+        [string]$nombreUsuario
+    )
+
+    # Solicitar nueva contraseña de forma segura
+    $newPass = Read-Host -Prompt "Ingrese nueva contraseña:" -AsSecureString
+    # Establecer la nueva contraseña y manejar cualquier error
+    try {
+        Set-ADAccountPassword -Identity $nombreUsuario -NewPassword $newPass -Reset
+        Write-Host "La nueva contraseña se establecio correctamente"
+    } catch {
+        Write-Host "Error al establecer la nueva contraseña: $_"
+    }
+}
+
 # Verificar que se proporcionen los argumentos esperados
 if ($args.Count -lt 2) {
     cls
-    Write-Host "Uso: .\script.ps1 <acción> <nombreUsuario>"
+    Write-Host "NOMBRE"
+    Write-Host "    ADuser.ps1"
     Write-Host ""
-    Write-Host "Acciones disponibles:"
-    Write-Host "        chk :  Verificar estado e informacion basica"
-    Write-Host "        ul  :  Desbloquea usuario en AD"
-    Write-Host "        mem  : Mostrar grupos de pertenencia"
-    Write-Host "        pw  :  Cambio de contraseña"
+    Write-Host "SINTAXIS"
+    Write-Host "    ADuser.ps1 <accion> <nombreUsuario> (solo la parte anterior al @)"
+    Write-Host ""
+    Write-Host "ACCIONES"
+    Write-Host "    chk :   Desbloquear y mostrar informacion basica"
+    Write-Host "    ul  :   Solo desbloquear usuario"
+    Write-Host "    mem :   Mostrar grupos de pertenencia (Miembro de)"
+    Write-Host "    pw  :   Cambio de contraseña en AD"
     Write-Host ""
     exit
 }
@@ -40,63 +118,14 @@ $nombreUsuario = $args[1]
 # Validar que la accion proporcionada sea valida
 if ($accion -notin @('chk', 'ul', 'mem', 'pw')) {
     cls
-    Write-Host "La acción proporcionada no es valida..."
+    Write-Host "La accion proporcionada no es valida..."
     exit
 }
 
+# Ejecutar la funcion correspondiente segun la accian proporcionada
 switch ($accion) {
-    'chk' {
-        cls
-        Write-Host "Verificar informacion del usuario:"
-        # Buscar las propiedades que nos interesan
-        $usuario = Get-ADUser -Identity $nombreUsuario -Properties EmployeeID, GivenName, Surname, UserPrincipalName, Enabled, Lockedout, PasswordExpired, PasswordLastSet, Created, AccountExpirationDate
-
-        if ($usuario.LockedOut) {
-            Write-Host "El usuario esta bloqueado. Desbloqueando..."
-            Unlock-ADAccount -Identity $nombreUsuario
-            Write-Host "Usuario desbloqueado."
-        }
-
-        # Mostrar información del usuario
-        $usuario | Select-Object EmployeeID, GivenName, Surname, UserPrincipalName, Enabled, Lockedout, PasswordExpired, PasswordLastSet, Created, AccountExpirationDate
-    } 
-    'ul' {
-        cls
-        Write-Host "Desbloquear $nombreUsuario en AD"
-        Unlock-ADAccount -Identity $nombreUsuario
-        # Esperar antes de verificar el estado
-        Start-Sleep -Seconds 2
-        Write-Host ""
-        # Obtener y mostrar el estado del usuario
-        $userStatus = Get-ADUser -Identity $nombreUsuario -Properties Lockedout | Select-Object -ExpandProperty Lockedout
-
-        if ($userStatus -eq $false) {
-            Write-Host "Usuario desbloqueado exitosamente"
-        } else {
-            Write-Host "Ha habido un error, volver a intentarlo"
-        }
-    }
-    'mem' {
-        cls
-        Write-Host "Grupos a los que pertenece:"
-        # Buscar los grupos de los que es miembro el usuario
-        $memberOfGroups = Get-ADUser -Identity $nombreUsuario -Properties MemberOf | Select-Object -ExpandProperty MemberOf
-        # Mostrar los nombres de los grupos
-        foreach ($group in $memberOfGroups) {
-            $groupName = ($group -split ',')[0] -replace '^CN='
-            Write-Host "- $groupName"
-        }
-    }
-     
-    'pw' {
-        # Solicitar nueva contraseña de forma segura
-        $newPass = Read-Host -Prompt "Ingrese nueva contraseña:" -AsSecureString
-        # Establecer la nueva contraseña y manejar cualquier error
-        try {
-            Set-ADAccountPassword -Identity $nombreUsuario -NewPassword $newPass -Reset
-            Write-Host "La nueva contraseña establecida correctamente"
-        } catch {
-            Write-Host "Error al establecer la nueva contraseña: $_"
-        }
-    }
+    'chk' { VerificarUsuario $nombreUsuario }
+    'ul' { DesbloquearUsuario $nombreUsuario }
+    'mem' { MostrarGruposPertenencia $nombreUsuario }
+    'pw' { CambiarContraseña $nombreUsuario }
 }
